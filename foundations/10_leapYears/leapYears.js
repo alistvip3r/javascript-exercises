1f8d367dd96ff665d10ed1957c6581cd540e983e
alistvip3r <251541935+alistvip3r@users.noreply.github.com>
2026-01-20 16:28:19 +0530

    Simplified the code for test execution

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,6 @@
-const leapYears = function() {
-
-};
-
+const leapYears = function(getYear) {
+    let year = Number(getYear); // Convert it to Number.
+    return ((year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0));
+}
 // Do not edit below this line
 module.exports = leapYears;
