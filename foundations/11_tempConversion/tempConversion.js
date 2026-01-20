8ce011caeb8e4e67c54d9ad4cd85155e5bd43891
alistvip3r <251541935+alistvip3r@users.noreply.github.com>
2026-01-20 18:24:31 +0530

    feature: Convert temperature to fahrenheit

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -4,8 +4,10 @@ const convertToCelsius = function(temp) {
   return Math.round(tempInCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
-  
+const convertToFahrenheit = function(temp) {
+  const tempConverted = Number(temp);
+  const tempInFahrenheit = tempConverted * 9 / 5 + 32;
+  return Math.round(tempInFahrenheit * 10) / 10;
 }
 
 // Do not edit below this line
