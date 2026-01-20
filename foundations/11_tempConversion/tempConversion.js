bbdd8119e281a904845391e21c85236eb2ffcfe0
alistvip3r <251541935+alistvip3r@users.noreply.github.com>
2026-01-20 18:03:39 +0530

    feature : Convert temperature to celsius

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const tempConverted = Number(temp);
+  const tempInCelsius = (tempConverted - 32) * 5 / 9;
+  return Math.round(tempInCelsius * 10) / 10;
 };
 
 const convertToFahrenheit = function() {
-};
+  
+}
 
 // Do not edit below this line
 module.exports = {
